4b69ba999d8729f834d729dbf952949cd8ce1091
peninah-njeri <peninahgathuru@gmail.com>
2019-10-06 20:31:28 +0300

    run a test to test saving user details

--- user_credential_test.py
+++ user_credential_test.py
@@ -1,2 +1,25 @@
 import unittest
 from user import user
+
+
+class TestUserCredentials(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Set a fake user and credential for each test
+        """
+        self.test_user = User('ninah', 'njeri', '123')
+        User.users_list.append(self.test_user)
+
+      
+    def test_create_user(self):
+        new_contact = User('pesh', 'nje', '0000')
+        self.assertEqual(new_contact.first_name, 'pesh')
+        self.assertEqual(new_contact.last_name, 'nje')
+        self.assertEqual(new_contact.password, '0000')
+
+    def test_save_user(self):
+        User.save_user(self.test_user)
+        has_user = self.test_user in User.users_list
+        self.assertTrue(has_user)    
+
