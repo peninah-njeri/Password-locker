e75e461f7dcb20d1cded0d43de6d58cfd68d5c42
peninah-njeri <peninahgathuru@gmail.com>
2019-10-06 20:24:15 +0300

    create an empty list array to store user details

--- user.py
+++ user.py
@@ -1,2 +1,14 @@
 class User:
+    users_list = []
     
+    def __init__(self, first_name, last_name, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+
+
+
+
+
+
+     
